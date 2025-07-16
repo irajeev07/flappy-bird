52fd73751b6b9c38ae3bba5eae92a262bef0f3ec
irajeev07 <bluelotus2438@gmail.com>
2025-07-16 21:25:49 +0545

    updating script

--- script.js
+++ script.js
@@ -9,12 +9,50 @@ canvas.height = window_height;
 canvas.style.background = "#a52f2fff";
 
 let context = canvas.getContext("2d");
+
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX =window_width;
+let pipeY = 0;
+
+
+let velocityX = -2; 
+
 const pipe = new Image();
 pipe.src = './pipe.png';
 
-pipe.onload= () => {
-    context.drawImage(pipe, 0, 0);
+requestAnimationFrame(update);
+setInterval(placePipes, 1500);
+
+pipe.onload= function() {
+      context.drawImage(pipe, 0, 0);
 }
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0,0,window.width,window.height);
+
+    for(let i=0; i< pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height); 
+    }
+}
+
+function placePipes() {
+    let pipe = {
+        img : pipes,
+        x : pipeX,
+        y : pipeY,
+        width : pipeWidth,
+        height : pipeHeight, 
+        passed : false
+    }
+
+    pipeArray.push(pipe);  
+}
+
 
 
 // context.fillStyle = "black";
