835c1a089a373e1ea672c7e606a07768905f293f
irajeev07 <bluelotus2438@gmail.com>
2025-07-16 14:58:46 +0545

    flappy bird

---
+++ script.js
@@ -0,0 +1,19 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.background="#C8C8C8";
+
+const pipe = new Image();
+pipe.src = 'pipe.png';
+ ctx.drawImage(img, 0, 0);
+
+
+// context.fillStyle = "black";
+//     context.fillRect(350, 0, 100, 250);
+//     context.fillRect(350, 400, 100, canvas.height - 100);
